d8cd0ad8928320aff3e610d7cc558b9af5e8ff0a
Heritier Kafindo <heritierkafindo@protonmail.com>
2019-03-25 12:57:23 -0400

    i modified snake.html and snake.js

--- snake.js
+++ snake.js
@@ -4,6 +4,14 @@
         const SNAKE_COLOUR ='lightgreen';
         const SNAKE_BORDER_COLOUR ='darkgreen';
 
+        let snake = [
+            {x: 150, y:150},
+            {x: 140, y:150},
+            {x: 130, y:150},
+            {x: 120, y:150},
+            {x: 110, y:150}
+        ]
+
         var gameCanvas = document.getElementById("gameCanvas");
 
         var ctx = gameCanvas.getContext("2d");
@@ -15,3 +23,22 @@
         ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 
         ctx.strokeRect(0, 0,gameCanvas.width, gameCanvas.height);
+
+        drawSnake();
+
+
+        function drawSnake(){
+
+            snake.forEach(drawSnakePart)
+        }
+
+        function drawSnakerPart(snakePart){
+
+            ctx.fillStyle = SNAKE_COLOUR;
+
+            ctx.strokestyle = SNAKE_BORDER_COLOUR;
+
+            ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+
+            ctx.sttrokeRect(snakePart.x, snakePart.y, 10, 10);
+        }
